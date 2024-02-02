f072a5d513c207577c267e6e6fcc8e6f97f4de1a
Marco Argento <silvermarco96@gmail.com>
2024-02-02 20:15:43 +0100

    Creazione funzioni per aggiungere immagini e creazione localStorage

--- src/declarations.ts
+++ src/declarations.ts
@@ -1,7 +1,18 @@
 /** @format */
 
 export const mapNodes = {
-  root: 'root',
+  container: 'container',
+  contCard: 'cont-card',
+  urlImg: 'url-img',
+  titImg: 'tit-img',
+  commImg: 'comm-img',
+  addImg: 'add-img',
 } as const;
 
 export type NodeID = (typeof mapNodes)[keyof typeof mapNodes];
+
+export type Immagine = {
+  src: string;
+  title: string;
+  comment: string;
+};
