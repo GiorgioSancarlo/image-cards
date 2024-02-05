eaa9843c0827a0e8079a09e169c314832b94428e
Marco Argento <silvermarco96@gmail.com>
2024-02-05 09:15:53 +0100

    Aggiunta preferiti e altre funzioni

--- src/utilities/addEventListener.ts
+++ src/utilities/addEventListener.ts
@@ -1,7 +1,12 @@
 import { NodeID } from '../declarations';
-import { utilityGetNode } from './getNode';
+import { utilityGetClass, utilityGetNode } from './getNode';
 
 export function utilityAddEventListener(nodeID: NodeID, event: keyof HTMLElementEventMap, handler: () => void) {
   const node = utilityGetNode(nodeID);
   node.addEventListener(event, handler);
+}
+
+export function utilityAddEventListenerForClass(nodeID: NodeID, event: keyof HTMLElementEventMap, handler: () => void) {
+  const node = utilityGetClass(nodeID);
+  for (let i = 0; i < node.length; i++) node[i].addEventListener(event, handler);
 }
